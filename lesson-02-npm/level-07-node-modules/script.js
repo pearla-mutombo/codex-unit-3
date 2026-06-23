80accbf344a459675bbd1e343f7fce3bd4f861a6
Pearla Mutombo <pmutombo1986@gmail.com>
2026-06-23 14:46:26 -0400

    completed level 5 and deleted duplicate name changes file for level-07

--- lesson-02-npm/level-07-node-modules/script.js
+++
@@ -1,3 +0,0 @@
-import chalk from 'chalk';
-
-console.log(chalk.green('Node modules building blocks for node applications and it holds all od the codes from the library that you install in your system.'));
