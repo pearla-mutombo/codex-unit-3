2e4b8375c7a4ba78d3fa03c8edb24bf78c4859e1
Pearla Mutombo <pmutombo1986@gmail.com>
2026-06-26 15:10:45 -0400

    corrected level 6 and level 10 in Lesson 2-npm

--- lesson-02-npm/level-06-npmjs-website/script.js
+++ lesson-02-npm/level-06-npmjs-website/script.js
@@ -1,5 +1,6 @@
-const catsInfo = [
-    "NPMJS website is a collection of node packages that you can install in your website.", "we founds a chalk a terminal styling string that allow you to add colors to your termial and styles your text in your terminal."
+const chalkInfo = [
+  "NPMJS website is a collection of node packages that you can install in your website.",
+  "we founds a chalk a terminal styling string that allows you to add colors to your termial and styles your text in your terminal.  Originally it was it was published 13years, then the lastest version was published 10months ago and the version is 5.6.2, and in the last 7days over 55million was dowloaded. It seems like it gets updated twice a year.",
 ];
 
-console.log(catsInfo);
+console.log(chalkInfo);
--- lesson-02-npm/level-10-gitignore/script.js
+++ lesson-02-npm/level-10-gitignore/script.js
@@ -1,1 +1,3 @@
-console.log("Hello My Name is Garfield!");
+console.log(
+  "gitignore, we use it to ignore files that we do not want to push to GitHub, for example 'node_modelues'. And secret token, such as 'API Keys', we don't want other to access our secured and sensitive information. Basically anything that you don't want in the cloud you ignore it. you don't push it.",
+);
