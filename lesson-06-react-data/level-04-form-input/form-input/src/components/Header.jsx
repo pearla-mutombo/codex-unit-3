b9626eeba00ee91a23f155609625bfd6ebf6d5ed
Pearla Mutombo <pmutombo1986@gmail.com>
2026-07-01 17:37:13 -0400

    completed level 4 of lesson 5

---
+++ lesson-06-react-data/level-04-form-input/form-input/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Render Data from a Form</h1>
+    </header>
+  );
+}
---
+++ lesson-06-react-data/level-04-form-input/form-input/src/components/Main.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+
+export default function Main() {
+  const [didMount, setDidMount] = useState(false);
+
+  const [values, setValues] = useState([]);
+
+  useEffect(componentDidMount, []);
+
+  return (
+    <main>
+      <h2>Main Content Area</h2>
+      <p>Component status: DidMount {didMount.toString()}</p>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Full Name:
+          <input name="fullName" type="text" />
+        </label>
+
+        <label>
+          Email:
+          <input name="email" type="email" />
+        </label>
+
+        <label>
+          Password:
+          <input name="password" type="password" />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <output>{values}</output>
+      <br />
+      <p>
+        How to use map to render Form Values after they are submitted: When the
+        form is submitted, we interperet the event and compile the input data
+        into an array of objects. We then use the JavaScript map method to loop
+        through this array, converting each raw data objects into interactive
+        details and summary React elements, which are injected into the state
+        and render in the DOM.
+      </p>
+    </main>
+  );
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formInputs = [
+      {
+        label: "FullName",
+        value: form.elements.fullName.value,
+      },
+
+      {
+        label: "Email",
+        value: form.elements.email.value,
+      },
+
+      {
+        label: "Password",
+        value: form.elements.password.value,
+      },
+    ];
+    const details = formInputs.map(toDetails);
+    setValues(details);
+  }
+
+  function toDetails(formInput, index) {
+    const details = (
+      <details key={index}>
+        <summary>{formInput.label}</summary>
+        <p>{formInput.value}</p>
+      </details>
+    );
+    return details;
+  }
+
+  function componentDidMount() {
+    setDidMount(true);
+  }
+}
